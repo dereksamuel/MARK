14c8061f218a36898fd71b7013e462185e4e21b9
Pipaplutarco <dereksamuelgr@gmail.com>
2021-12-20 16:58:02 -0500

    coords completed

--- index.js
+++ index.js
@@ -19,17 +19,28 @@ function calculateDistance({ objective, myPosition }) {
   const lat = (objective.latitude - myPosition.latitude) ** 2;
   const lon = (objective.longitude - myPosition.longitude) ** 2;
 
-  return Math.sqrt(lat + lon) * 1000;
+  return {
+    distance: Math.sqrt(lat + lon) * 1000,
+    lat,
+    lon
+  };
 }
 
 function initMARK() {
-  const distance = calculateDistance({
+  const {
+    distance,
+    lat,
+    lon
+  } = calculateDistance({
     objective: window.objective,
     myPosition: window.myPosition,
   });
 
+  const ejeX = lon < 0 ? "izquierda" : "derecha";
+  const ejeY = lat < 0 ? "ve atras" : "sigue derecho";
+
   $panel.innerHTML = `
-    <strong>Gira a la derecha</strong>
+    <strong>Ve a la ${ejeX}, ${ejeY}</strong>
     ${distance.toFixed(3)}m
   `;
 }
